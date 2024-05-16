60c1400de4ef5ed1baf760c968b5ce47c39ca2ea
tomyItSky <tomy.agung@skyparking.co.id>
2024-05-17 00:17:41 +0700

    .

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
   //   domains: ["localhost"],
   // },
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://147.139.135.195:8090/api/:path*", // Ganti dengan URL server Anda
+      },
+    ];
+  },
 };
 
 export default nextConfig;
